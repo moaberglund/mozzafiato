1ee1f7fa82898ffb8089840d42831db3bfd5da9e
moaberglund <moa.21@hotmail.com>
2025-01-08 14:48:14 +0100

    tom tagg

--- front-page.php
+++ front-page.php
@@ -35,7 +35,7 @@
                 <div id="room-section">
                     <h2>Our Rooms & Suites</h2>
                     <div class="grid">
-                        < class="homeRoom">
+                        <div class="homeRoom">
                             <?php
                             //finns bild?
                             if (has_post_thumbnail()) {
@@ -48,41 +48,76 @@
                             <?php } ?>
 
 
+                        </div>
+
+
                     </div>
 
-
-                </div>
-
-                <?php
+                    <?php
             }
         }
         ?>
 
-        <div class="center">
-            <!-- RÄTT LÄNK HÄR -->
-            <button>View all rooms</button>
-        </div>
+            <div class="center">
+                <!-- RÄTT LÄNK HÄR -->
+                <button>View all rooms</button>
+            </div>
 
 
 
 
 
-        <div id="parallaxHome" class="parallax">
+            <div id="parallaxHome" class="parallax">
 
-        </div>
+            </div>
 
-        <!-- section for news articles -->
-        <?php
-        query_posts('category_name=news&posts_per_page=3');
-        if (have_posts()) {
-            while (have_posts()) {
-                the_post(); ?>
+            <!-- section for news articles -->
+            <?php
+            query_posts('category_name=news&posts_per_page=3');
+            if (have_posts()) {
+                while (have_posts()) {
+                    the_post(); ?>
 
-                <div>
-                    <h2>News</h2>
-                    <div class="flex">
-                        <div class="news-bubble">
-                            <a href="<?php the_permalink(); ?>">
+                    <div>
+                        <h2>News</h2>
+                        <div class="flex">
+                            <div class="news-bubble">
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php
+                                    //finns bild?
+                                    if (has_post_thumbnail()) {
+                                        ?>
+                                        <!-- dynamisk bild -->
+                                        <?php the_post_thumbnail('img-square') ?>
+                                        <h3><?php the_title(); ?></h3>
+                                    </a>
+
+                                <?php } ?>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                    <?php
+                }
+            }
+            ?>
+
+            <!-- services (max:6) -->
+
+            <?php
+            query_posts('category_name=services&posts_per_page=6');
+            if (have_posts()) {
+                while (have_posts()) {
+                    the_post(); ?>
+
+                    <div id="services">
+                        <h2>Our Services</h2>
+                        <div class="flex">
+                            <div class="service-bubble">
+
                                 <?php
                                 //finns bild?
                                 if (has_post_thumbnail()) {
@@ -90,56 +125,21 @@
                                     <!-- dynamisk bild -->
                                     <?php the_post_thumbnail('img-square') ?>
                                     <h3><?php the_title(); ?></h3>
-                                </a>
+                                    <p><?php the_content(); ?></p>
 
-                            <?php } ?>
+                                <?php } ?>
 
+                            </div>
                         </div>
+
+
                     </div>
 
-
-                </div>
-
-                <?php
+                    <?php
+                }
             }
-        }
-        ?>
-
-        <!-- services (max:6) -->
-
-        <?php
-        query_posts('category_name=services&posts_per_page=6');
-        if (have_posts()) {
-            while (have_posts()) {
-                the_post(); ?>
-
-                <div id="services">
-                    <h2>Our Services</h2>
-                    <div class="flex">
-                        <div class="service-bubble">
-
-                            <?php
-                            //finns bild?
-                            if (has_post_thumbnail()) {
-                                ?>
-                                <!-- dynamisk bild -->
-                                <?php the_post_thumbnail('img-square') ?>
-                                <h3><?php the_title(); ?></h3>
-                                <p><?php the_content(); ?></p>
-
-                            <?php } ?>
-
-                        </div>
-                    </div>
-
-
-                </div>
-
-                <?php
-            }
-        }
-        ?>
-    </div>
+            ?>
+        </div>
 
 
 </main>
