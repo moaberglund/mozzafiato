59a21032a08756bcb5bf37e034b62be631596b76
moaberglund <moa.21@hotmail.com>
2025-01-10 10:35:58 +0100

    nestlad navigation

--- js/navigation.js
+++ js/navigation.js
@@ -20,3 +20,23 @@ function toggleMenu() {
         navMenuEl.classList.add("open");
     }
 }
+
+
+document.querySelectorAll("#burger-nav .menu-item-has-children > a").forEach((parentLink) => {
+    parentLink.addEventListener("click", (e) => {
+        e.preventDefault(); // Hindra länkens standardbeteende
+
+        const subMenu = parentLink.nextElementSibling; // Hämta underlänken (.sub-menu)
+
+        if (subMenu.classList.contains("visible")) {
+            subMenu.classList.remove("visible"); // Dölj underlänken
+        } else {
+            // Dölj andra öppna underlänkar (om du vill ha en "accordion"-effekt)
+            document.querySelectorAll("#burger-nav .sub-menu.visible").forEach((menu) => {
+                menu.classList.remove("visible");
+            });
+
+            subMenu.classList.add("visible"); // Visa aktuell underlänk
+        }
+    });
+});
