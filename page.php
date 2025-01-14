03533d480d3b6260546734e4449db7b1fea6b57b
moaberglund <moa.21@hotmail.com>
2025-01-14 11:57:04 +0100

    städ

--- page.php
+++ page.php
@@ -1,20 +1,15 @@
 <?php get_header(); ?>
-<!-- Sida för undersida ? behövs den ?? jag vet inte vad du gööör :') -->
- <!-- utvecklad för övre delen av contact atm -->
+<!-- utvecklad för övre delen av contact atm -->
 <main>
     <div id="contact" class="wrapper">
-
         <?php if (have_posts()) {
             while (have_posts()) {
                 the_post();
             }
         }
         ?>
-
         <h1><?php the_title(); ?></h1>
-
         <div class="grid">
-
             <div>
                 <?php
                 //finns bild?
@@ -30,22 +25,15 @@
 
                 <?php the_content(); ?>
             </div>
-
         </div>
         <!-- statiskt innehåll atm -->
         <div class="staff">
-            <img src="<?= get_template_directory_uri();?>/media/luisa.jpg" alt="Profile picture of Luisa">
+            <img src="<?= get_template_directory_uri(); ?>/media/luisa.jpg" alt="Profile picture of Luisa">
             <h3>Luisa</h3>
             <p>Hi! I'm the person behind Mozzafiato.</p>
         </div>
         <?php
-
-
         ?>
-
     </div>
-
-
 </main>
-
 <?php get_footer(); ?>
