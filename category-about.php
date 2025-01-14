3c4fddbd83bfee169a3160f8d7be43a736d597b3
moaberglund <moa.21@hotmail.com>
2025-01-14 12:03:43 +0100

    städ

--- category-about.php
+++ category-about.php
@@ -1,9 +1,7 @@
 <?php get_header(); ?>
-
 <main>
     <div id="about" class="wrapper">
         <h1>About</h1>
-
         <!-- about (max:5)-->
         <?php
         query_posts('category_name=about&posts_per_page=5');
@@ -24,29 +22,15 @@
                     <div class="about-bubble">
                         <h2><?php the_title(); ?></h2>
                         <p><?php the_content(); ?></p>
-
                     </div>
-
                 </div>
                 <div class="border-img">
-                    <img src="<?= get_template_directory_uri();?>/media/icons/divider.png" alt="Page divider">
-
-
+                    <img src="<?= get_template_directory_uri(); ?>/media/icons/divider.png" alt="Page divider">
                 </div>
-
                 <?php
             }
         }
         ?>
-
-        <!-- not working -->
-        <div id="diningtable" class="parallax">
-            <!-- parallax dining table -->
-
-
-        </div>
     </div>
-
 </main>
-
 <?php get_footer(); ?>
